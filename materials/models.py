f50e8ff04fd48fbc324cd120bb003ed9444a166e
starcev daniil <stdanmark@mail.ru>
2024-11-21 14:11:42 +0300

    24.1 is working

--- materials/models.py
+++ materials/models.py
@@ -3,6 +3,27 @@ from django.db import models
 NULLABLE = {"blank": True, "null": True}
 
 
+class Course(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+    preview = models.ImageField(
+        upload_to="materials/course",
+        **NULLABLE,
+        verbose_name="Превью",
+        help_text="Добавьте фото",
+    )
+    description = models.TextField(
+        **NULLABLE, verbose_name="Описание", help_text="Добавьте описание"
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
 class Lesson(models.Model):
     name = models.CharField(
         max_length=50, verbose_name="Название урока", help_text="Укажите название урока"
@@ -24,31 +45,10 @@ class Lesson(models.Model):
         verbose_name="ссылка на видео",
         help_text="Укажите ссылку на видео",
     )
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name="курсы", **NULLABLE
+    )
 
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
-
-
-class Course(models.Model):
-    name = models.CharField(
-        max_length=50,
-        verbose_name="Название курса",
-        help_text="Укажите название курса",
-    )
-    preview = models.ImageField(
-        upload_to="materials/course",
-        **NULLABLE,
-        verbose_name="Превью",
-        help_text="Добавьте фото",
-    )
-    description = models.TextField(
-        **NULLABLE, verbose_name="Описание", help_text="Добавьте описание"
-    )
-    lessons = models.ForeignKey(
-        Lesson, on_delete=models.CASCADE, verbose_name="уроки", **NULLABLE
-    )
-
-    class Meta:
-        verbose_name = "Курс"
-        verbose_name_plural = "Курсы"
