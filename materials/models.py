75406c0e5d942b576d78459f06b2074d4b102108
starcev daniil <stdanmark@mail.ru>
2024-11-21 03:57:36 +0300

    24.1 done

---
+++ materials/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=50, verbose_name="Название урока", help_text="Укажите название урока"
+    )
+    preview = models.ImageField(
+        upload_to="materials/lesson",
+        **NULLABLE,
+        verbose_name="Превью",
+        help_text="Добавьте фото",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        **NULLABLE,
+        help_text="Добавьте описание",
+    )
+    link = models.URLField(
+        max_length=200,
+        **NULLABLE,
+        verbose_name="ссылка на видео",
+        help_text="Укажите ссылку на видео",
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+    preview = models.ImageField(
+        upload_to="materials/course",
+        **NULLABLE,
+        verbose_name="Превью",
+        help_text="Добавьте фото",
+    )
+    description = models.TextField(
+        **NULLABLE, verbose_name="Описание", help_text="Добавьте описание"
+    )
+    lessons = models.ForeignKey(
+        Lesson, on_delete=models.CASCADE, verbose_name="уроки", **NULLABLE
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
