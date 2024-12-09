15521194486337d0087dec7123b53f7d4598fab0
starcev daniil <stdanmark@mail.ru>
2024-12-09 10:27:30 +0300

    26.2 done

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Payments
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_filter = ("id", "email")
+
+
+@admin.register(Payments)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'payment_date', 'paid_course', 'paid_lesson', 'amount', 'payment_method')
